3cee518c71c2a7f2d7e10ab594cb05c9e88931e2
CLnagisa <437155119@qq.com>
2017-05-04 21:18:10 +0800

    右边十字架的添加功能完成,用了alertDialog弹出框和向哈希表加入新数据

--- jikezuoye/app/src/main/java/ham/andy/jikezuoye/Main.java
+++ jikezuoye/app/src/main/java/ham/andy/jikezuoye/Main.java
@@ -21,6 +21,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -41,7 +42,7 @@ import java.util.Map;
 
 public class Main extends Activity {
     private PopupWindow popupWindow;  //从右出来的弹出框
-    private Context mContext = null;
+    private Context mContext = null;   //上下文
     private List<Map<String, Object>> myData;  //listitem用
     //List<Map<String, Object>> items = new ArrayList<Map<String, Object>>(); 是定义一个List类型的变量，list里面存放的是一个Map，而Map的key是一个String类型，Map的value是Object类型,Map是一个接口 代表一个key-value 键值对
     private List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();      //用全局来保存，实现增加和删除
@@ -56,9 +57,10 @@ public class Main extends Activity {
         actionBar.setCustomView(R.layout.main_title);   //获取自定义actionbar文件
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);   //显示出来
         setContentView(R.layout.main);
-        mContext = this;
 
-        /*点击头像右边弹出框的实现*/
+        mContext = this;    //获取主Main的this，有几个地方会用到
+
+        /********点击头像右边弹出框的实现***********/
         ImageView touxiang = (ImageView)findViewById(R.id.touxiang);
         touxiang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,7 +71,7 @@ public class Main extends Activity {
 
 
 
-        /*点击右边的十字架出现操作按钮*/
+        /********点击右边的十字架出现操作按钮*******/
         final ImageView caozuo = (ImageView)findViewById(R.id.caozuo);
         caozuo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,17 +81,16 @@ public class Main extends Activity {
         });
 
 
-        /*点击用户跳转到不同的聊天页面*/
-        //生成设配器，数组===》listItem
-        ListView listview = null;
+        /********点击用户跳转到不同的聊天页面*******/
+        ListView listview = null;              //生成设配器，数组===》listItem
         listview = (ListView)findViewById(R.id.MyListView);
 
-/*        SimpleAdapter adapter = new SimpleAdapter(this, getData(),   //数据来源
+        /*        SimpleAdapter adapter = new SimpleAdapter(this, getData(),   //数据来源
                                                   R.layout.my_listitem,   //ListItem的xml实现
                                                   new String[]{"title", "img"},   //动态数组与ListItem对应的子项
                                                   new int[]{R.id.title, R.id.img});  //ListItem的XML文件里面的id
         listview.setAdapter(adapter);//启动并显示设配器*/
-        /*用BaseAdapter并重写BaseAdapter类来实现*/
+        //用BaseAdapter并重写BaseAdapter类来实现
         myData = getData();
         MyAdapter adapter = new MyAdapter(this);
         listview.setAdapter(adapter);
@@ -101,11 +102,8 @@ public class Main extends Activity {
 
 
 
-
-
-
-
-
+    /**********************************************************右边弹出框相关的函数开始***************************************************************/
+    /**********************************************************右边弹出框相关的函数开始***************************************************************/
     /**
      * 添加新笔记时弹出的popWin关闭的事件，主要是为了将背景透明度改回来
      *
@@ -181,7 +179,11 @@ public class Main extends Activity {
         lp.alpha = bgAlpha; //0.0-1.0
         getWindow().setAttributes(lp);    //setAttributes设置透明属性
     }
+    /**********************************************************右边弹出框相关的函数结束***************************************************************/
+    /**********************************************************右边弹出框相关的函数结束***************************************************************/
 
+    /**********************************************************右边十字架弹出框的函数开始*************************************************************/
+    /**********************************************************右边十字架弹出框的函数开始*************************************************************/
     /*右边操作按钮的弹出框*/
     private void showPopupMenu(View view) {
         // View当前PopupMenu显示的相对View的位置
@@ -192,22 +194,39 @@ public class Main extends Activity {
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-
-                Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                //创建一个带输入框的弹出框
+                final EditText inputServer = new EditText(mContext);
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("请输入要添加的用户名").setView(inputServer)
+                        .setNegativeButton("取消", null);                                  //取消就取消
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {  //点确定的相应事件
+                    public void onClick(DialogInterface dialog, int which) {
+                        String yonghuming = null;
+                        yonghuming = inputServer.getText().toString();
+                        ListView listview = null;
+                        listview = (ListView)findViewById(R.id.MyListView);
+                        map = new HashMap<String, Object>();
+                        map.put("title", yonghuming);
+                        map.put("img", R.drawable.a);
+                        list.add(map);
+                        MyAdapter adapter = new MyAdapter(mContext);
+                        listview.setAdapter(adapter);
+                    }
+                });
+                builder.show();
                 return false;
             }
         });
         popupMenu.show();
     }
+    /**********************************************************右边十字架弹出框的函数结束*************************************************************/
+    /**********************************************************右边十字架弹出框的函数结束*************************************************************/
 
-
-
-
-
-
+    /**********************************************************主页面的数据显示和listview应用函数开始*************************************************/
+    /**********************************************************主页面的数据显示和listview应用函数开始*************************************************/
     /* 主页数据显示，listitem的函数实现*/
        private List<Map<String, Object>> getData() {
-        //添加数据用的，一个哈希map
+        //获取数据用的，一个哈希map
         map = new HashMap<String, Object>();
         map.put("title", "第一个人");
         map.put("img", R.drawable.a);
@@ -278,8 +297,8 @@ public class Main extends Activity {
         }
 
     }
-
-
+    /**********************************************************主页面的数据显示和listview应用函数结束*************************************************/
+    /**********************************************************主页面的数据显示和listview应用函数结束*************************************************/
 
 }
 
