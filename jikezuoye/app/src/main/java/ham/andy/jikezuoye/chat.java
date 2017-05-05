614f18fd097594da4ca380d7d4f0aa48fd80785c
CLnagisa <437155119@qq.com>
2017-05-05 16:46:50 +0800

    聊天记录的返回按钮,用finish（）这个函数结束掉了这个activity

--- jikezuoye/app/src/main/java/ham/andy/jikezuoye/chat.java
+++ jikezuoye/app/src/main/java/ham/andy/jikezuoye/chat.java
@@ -6,10 +6,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -25,10 +28,27 @@ public class chat extends Activity{
         setContentView(R.layout.chat);
         getActionBar().hide();  //不显示actionBar
 
+        /**********更改标题*********/
         Intent intent = getIntent();    //获取传过来的数据
         String aaa = intent.getStringExtra("title");
         TextView chat_title = (TextView)findViewById(R.id.chat_title);//根据传过来的数据更改标题
         chat_title.setText(aaa);
+
+        /**********放回按钮*********/
+        TextView fanghui = (TextView)findViewById(R.id.chat_fanhui);
+        fanghui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ImageView fanghui1 = (ImageView)findViewById(R.id.chat_img);
+        fanghui1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
     }
